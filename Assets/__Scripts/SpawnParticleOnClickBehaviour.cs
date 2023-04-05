ee20f65239214684e87b21b71cc658fc1815030c
Asbel <tchatrefou@gaming.tech>
2023-04-05 13:38:35 +0200

    small fixes for 1.0 build
    touch screen feedback
    lights

--- Assets/__Scripts/SpawnParticleOnClickBehaviour.cs
+++ Assets/__Scripts/SpawnParticleOnClickBehaviour.cs
@@ -12,18 +12,36 @@ public class SpawnParticleOnClickBehaviour : MonoBehaviour
 
     private GameObject m_PreviousParticle;
 
+    private BasicActions basicActions;
+
+    private void Awake()
+    {
+        basicActions= new BasicActions();
+        basicActions.UserInteraction.Click.performed += OnClick;
+    }
+
     void Start()
     {
         m_MainCamera = Camera.main;
     }
 
-    void OnClick()
+    void OnClick(InputAction.CallbackContext context)
     {
-        if(m_PreviousParticle)
+        if(m_PreviousParticle && m_ForceDestroyPrevious)
             Destroy(m_PreviousParticle);
 
-        Vector3 mousePos = Mouse.current.position.ReadValue();
-        Vector3 particleWorldPos = m_MainCamera.ScreenToWorldPoint(mousePos + Vector3.forward * 10);
-        m_PreviousParticle = Instantiate(m_ParticlePrefab, particleWorldPos, Quaternion.identity, transform);
+        Vector2 screenPos = basicActions.UserInteraction.Cursor.ReadValue<Vector2>();
+        Vector3 particleWorldPos = m_MainCamera.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, 0.2f));
+        m_PreviousParticle = Instantiate(m_ParticlePrefab, particleWorldPos, Quaternion.identity, m_MainCamera.transform);
+    }
+
+    private void OnEnable()
+    {
+        basicActions.UserInteraction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        basicActions.UserInteraction.Disable();
     }
 }
